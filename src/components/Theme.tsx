a4ae4532e911aa553b1872d19b5b1bc771ffc1de
Sandeep-morya <saabmaurya@gmail.com>
2024-02-18 00:12:02 +0530

    updated theme

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -23,11 +23,10 @@ const Theme = () => {
     return (
         <div
             className={twMerge(
-                "fixed left-0 bottom-10 transition duration-500  flex px-2 gap-4 bg-white/10 backdrop-blur-sm z-50 rounded-r-full shadow -translate-x-[4.5rem]",
+                "fixed left-0 bottom-10 transition duration-500  flex gap-4 bg-white/10 backdrop-blur-sm z-50 rounded-r-full shadow -translate-x-[4.5rem]",
                 showTheme && "translate-x-0",
             )}>
-            {/*  */}
-            <div className="h-8  flex justify-end items-center">
+            <div className="h-8  flex pl-1  items-center">
                 <a href="https://carezonelko.com/" className="flex justify-center items-center h-full">
                     <div style={{ backgroundColor: themes.default.primary }} className="h-1/2 aspect-square rounded-full translate-x-1" />
                     <div style={{ backgroundColor: themes.default.secondary }} className="h-1/2 aspect-square rounded-full bg-secondary" />
@@ -36,7 +35,9 @@ const Theme = () => {
                     <div style={{ backgroundColor: themes.new.primary }} className="h-1/2 aspect-square rounded-full translate-x-1 bg-primary" />
                     <div style={{ backgroundColor: themes.new.secondary }} className="h-1/2 aspect-square rounded-full bg-secondary" />
                 </a>
-                <div onClick={toggle} className={twMerge("transition-all duration-500", showTheme && "rotate-180")}><PiCaretRightBold /></div>
+                <div onClick={toggle} className={twMerge("transition-all px-2 duration-500 hover:text-primary", showTheme && "rotate-180")}>
+                    <PiCaretRightBold />
+                </div>
             </div>
         </div>
     );
